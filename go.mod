57d9d6cd47b3347a13f4486f534813dcafb88c0f
hiroky1983 <hirockysan1983@gmail.com>
2024-03-24 15:24:34 +0900

    コミット

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module dq-app
 
 go 1.21.1
+
+require github.com/aws/aws-sdk-go v1.51.6
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
--- main.go
+++ main.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
+
+func main() {
+	ddb := dynamodb.New(session.New(), aws.NewConfig().WithRegion("ap-northeast-1"))
+
+	prams := &dynamodb.CreateTableInput{
+		AttributeDefinitions: []*dynamodb.AttributeDefinition{
+			{
+				AttributeName: aws.String("id"),
+				AttributeType: aws.String("S"),
+			},
+		},
+		TableName: aws.String("test_table"),
+		KeySchema: []*dynamodb.KeySchemaElement{
+			{
+				AttributeName: aws.String("id"),
+				KeyType: aws.String("HASH"),
+			},
+		},
+	}
+	resp, err := ddb.CreateTable(prams)
+	if err != nil {
+		panic(err)
+	}
+	println(resp)
+}
